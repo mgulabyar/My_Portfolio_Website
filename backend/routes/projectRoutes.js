2a5b64d8b2f16a2fa55c405b2f53b072f931452b
M Gulab Yar <mgulabyaarmgulabyaar@gmail.com>
2026-06-12 17:14:29 +0500

    feat: define public and private routes for project showcase

---
+++ backend/routes/projectRoutes.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const router = express.Router();
+const { createProject, getProjects, deleteProject } = require('../controllers/projectController');
+const { protect } = require('../middleware/authMiddleware');
+
+router.get('/', getProjects);
+
+router.post('/', protect, createProject);
+router.delete('/:id', protect, deleteProject);
+
+module.exports = router;
