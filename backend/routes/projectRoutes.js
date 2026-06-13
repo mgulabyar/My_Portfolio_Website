b165f16facdfd9f7f64c1e320e20ad6a1d33fe84
M Gulab Yar <mgulabyaarmgulabyaar@gmail.com>
2026-06-13 10:15:57 +0500

    feat: register secure PUT routes

--- backend/routes/projectRoutes.js
+++ backend/routes/projectRoutes.js
@@ -3,12 +3,10 @@ const router = express.Router();
 const { createProject, getProjects, updateProject, deleteProject } = require('../controllers/projectController');
 const { protect } = require('../middleware/authMiddleware');
 
-// Public Route
 router.get('/', getProjects);
 
-// Private Routes
 router.post('/', protect, createProject);
-router.put('/:id', protect, updateProject); // PUT route register karein
+router.put('/:id', protect, updateProject); 
 router.delete('/:id', protect, deleteProject);
 
 module.exports = router;
