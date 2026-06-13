9962e439a665e05af04550ed8e537c8a24ba46e7
M Gulab Yar <mgulabyaarmgulabyaar@gmail.com>
2026-06-13 10:10:31 +0500

    feat: define public and private routes for global profile configuration sessions

--- backend/routes/profileRoutes.js
+++ backend/routes/profileRoutes.js
@@ -3,8 +3,7 @@ const router = express.Router();
 const { getProfile, updateProfile } = require('../controllers/profileController');
 const { protect } = require('../middleware/authMiddleware');
 
-// Define Routes
 router.get('/', getProfile);
-router.put('/', protect, updateProfile); // Admin strictly required
+router.put('/', protect, updateProfile); 
 
 module.exports = router;
