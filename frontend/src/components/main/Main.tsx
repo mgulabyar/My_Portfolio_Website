79dc86ca207fa09cb044312cce591f447dd75f66
M Gulab Yar <mgulabyaarmgulabyaar@gmail.com>
2026-06-14 08:57:48 +0500

    configure main modular wrapper layout bounds

--- frontend/src/components/main/Main.tsx
+++ frontend/src/components/main/Main.tsx
@@ -1,13 +1,13 @@
 import Hero from './Hero';
 // import About from './About';
-import Contact from './Contact'; // Contact import karein
+import Contact from './Contact';
 
 export default function Main() {
   return (
     <div className="relative z-10 max-w-350 mx-auto px-6 md:px-12">
       <Hero />
       {/* <About /> */}
-      <Contact /> {/* Contact section register karein */}
+      <Contact />
     </div>
   );
 }
