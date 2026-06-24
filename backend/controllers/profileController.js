42f80ef39bdb4a50870a09710fd3bfdcd2ba7c4d
M Gulab Yar <mgulabyaarmgulabyaar@gmail.com>
2026-06-24 10:28:00 +0500

    feat: develop secure admin registration controller endpoint

--- backend/controllers/profileController.js
+++ backend/controllers/profileController.js
@@ -11,7 +11,6 @@ exports.getProfile = async (req, res) => {
                 data: null
             });
         }
-// no wi'm working in profileconorljs
         res.status(200).json({
             success: true,
             data: profile
